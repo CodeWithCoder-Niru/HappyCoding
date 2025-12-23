9e4ee2e7426b752798080b4cb62f8be8071f8067
Niranjan <71616863+CodeWithCoder-Niru@users.noreply.github.com>
2025-12-24 00:09:08 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -7,10 +7,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBbM6c9r5NIZ_0FjzmBqhx8vTbgCTUsM-I",
+  authDomain: "codelock-4db77.firebaseapp.com",
+  projectId: "codelock-4db77",
+  storageBucket: "codelock-4db77.firebasestorage.app",
+  messagingSenderId: "457021445542",
+  appId: "1:457021445542:web:4f72fb4a110b1888642b67"
 };
 
 const app = initializeApp(firebaseConfig);
